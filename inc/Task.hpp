d8471797e19117b77c58c2090dc1ee9ca763f186
João Ceia <joao.p.ceia@gmail.com>
2022-03-24 18:42:28 +0000

    added dependencies to task

--- inc/Task.hpp
+++ inc/Task.hpp
@@ -6,21 +6,38 @@
 /*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 21:20:11 by jpceia            #+#    #+#             */
-/*   Updated: 2022/03/11 01:46:47 by jpceia           ###   ########.fr       */
+/*   Updated: 2022/03/24 18:40:25 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TASK_HPP
 # define TASK_HPP
 
+# include <set>
 # include <pthread.h>
 # include "Mutex.hpp"
+
 
 class Task
 {
 public:
-    virtual ~Task() {};
+    typedef std::set<Task *> Set;
+    Task();
+    virtual ~Task();
     virtual void run() = 0;
+
+    void add_dependency(Task *task);
+    bool has_dependencies() const;
+    Set getDependents() const;
+
+private:
+    void _add_dependent(Task *task);
+    void _remove_dependency(Task* task);
+
+    // Private attributes
+    Set _dependencies;
+    Set _dependents;
+    Mutex _mutex; // for dependencies
 };
 
 #endif
---
+++ src/Task.cpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Task.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/20 21:33:08 by jpceia            #+#    #+#             */
+/*   Updated: 2022/03/24 18:40:39 by jpceia           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Task.hpp"
+#include "LockGuard.hpp"
+#include <iostream>
+
+Task::Task()
+{
+}
+
+Task::~Task()
+{
+    for (Set::const_iterator it = _dependents.begin();
+        it != _dependents.end(); ++it)
+        (*it)->_remove_dependency(this);
+}
+
+void Task::add_dependency(Task *task)
+{
+    _dependencies.insert(task);
+    task->_add_dependent(this);
+}
+
+void Task::_add_dependent(Task *task)
+{
+    LockGuard(task->_mutex);
+    task->_dependents.insert(task);
+}
+
+bool Task::has_dependencies() const
+{
+    return !_dependencies.empty();
+}
+
+void Task::_remove_dependency(Task *task)
+{
+    if (!_dependencies.erase(task))
+        std::cerr << "Task::_remove_dependency: task not found" << std::endl;
+}
